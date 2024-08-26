7acc2af9b7e9f9a274b7c479930dae05b3ca3c49
Danielle Sanches <128653080+DanySanches@users.noreply.github.com>
2024-08-26 06:50:24 -0300

    Update AnimalVoadorAB.java

--- src/Lista4/AnimalVoadorAB.java
+++ src/Lista4/AnimalVoadorAB.java
@@ -3,7 +3,7 @@ package Lista4;
 public abstract class AnimalVoadorAB extends AnimalAB {
 	
 	protected int quantidadeAsas;
-    protected double envergaduraAsas;
+    	protected double envergaduraAsas;
 
 	protected int velocidade;
 	
